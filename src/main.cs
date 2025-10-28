5e35e8500a7c3237d7e4290e976193e064fe431b
Lieslot <rlaxodud1999@gmail.com>
2025-10-28 19:03:50 +0900

    display error message

--- src/main.cs
+++ src/main.cs
@@ -3,5 +3,11 @@ class Program
     static void Main()
     {
         Console.Write("$ ");
+        
+        // ユーザーのinputを受け取る
+        string input = Console.ReadLine();
+
+
+        Console.Write(input + ": command not found");        
     }
 }
